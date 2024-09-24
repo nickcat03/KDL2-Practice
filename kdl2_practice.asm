9a44aea0cb4fd9bbaf0ddd2d084f99759275c4b9
nickcat03 <nc.catapano@gmail.com>
2024-09-23 21:52:35 -0400

    exit any level

--- kdl2_practice.asm
+++ kdl2_practice.asm
@@ -1,10 +1,12 @@
 ; KDL2 Practice Hack
 
-;INCSRC "src/hex_edits.asm"
-
 ; Always send boss doors to bossfight instead of bonus game
 SECTION "check_boss_status", ROMX[$4559],BANK[8]
 ld a, 0
+nop
+
+SECTION "always_allow_stage_exit", ROMX[$61A1],BANK[$0F]
+ld a, 1
 nop
 
 ;lives: 0084 and 1f11 cartram
